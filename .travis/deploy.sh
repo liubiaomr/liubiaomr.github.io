01c2e0fa59b2af5c3c2aed65b48036c0082952a7
liubiaomr <liubiaomr@163.com>
2018-12-24 03:53:51 +0800

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,13 +3,13 @@ set -ev
 export TZ='Asia/Shanghai'
 
 # 先 clone 再 commit，避免直接 force commit
-git clone -b master git@github.com:liubiaomr/liubiaomr.github.io.git .deploy_git
+git clone -b master git@github.com:liubiaomr/liubiaomr.github.io.git _config.yml
 
-cd .deploy_git
-git checkout master
-cp -r ../public/* ./
+# cd .deploy_git
+# git checkout master
+# cp -r ../public/* ./
 
-git add .
-git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+# git add .
+# git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
 
-git push origin master:master --force --quiet
+# git push origin master:master --force --quiet
