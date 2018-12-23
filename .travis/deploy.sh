3b55ac06e7e9412dd9465ca1c3fdd2955438f5f1
Teddy <liubiaomr@163.com>
2018-12-24 03:13:50 +0800

    修改Travis配置文件

---
+++ .travis/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ev
+export TZ='Asia/Shanghai'
+
+# 先 clone 再 commit，避免直接 force commit
+git clone -b master git@github.com:liubiaomr/liubiaomr.github.io.git .deploy_git
+
+cd .deploy_git
+git checkout master
+cp -r ../public/* ./
+
+git add .
+git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+
+git push origin master:master --force --quiet
