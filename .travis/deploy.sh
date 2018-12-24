4dd01b047426d5e65ccdc9ada2a4befc69cae847
gkbf <gkb@1440775055.com>
2018-12-24 11:28:14 +0800

    修改travis配置文件，删除.travis文件夹下的11文件

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,13 +3,18 @@ set -ev
 export TZ='Asia/Shanghai'
 
 # 先 clone 再 commit，避免直接 force commit
-git clone -b master git@github.com:liubiaomr/liubiaomr.github.io.git _config.yml
+# git clone -b master git@github.com:liubiaomr/liubiaomr.github.io.git .deploy_git
 
-# cd .deploy_git
-# git checkout master
-# cp -r ../public/* ./
+cd .deploy_git
+git checkout master
+mv .git/ ../public/
+cd ../public
 
-# git add .
-# git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+git add .
+git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
 
-# git push origin master:master --force --quiet
+# 同时 push 一份到自己的服务器上
+git remote add vps git@47.52.116.180:/var/repo/blog.git
+
+git push vps master:master --force --quiet
+git push origin master:master --force --quiet
